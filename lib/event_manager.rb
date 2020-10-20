341584d309ef67dfb5c937b04cd9b9b81d137e4b
Anthony Giles <omgliekpie@gmail.com>
2020-10-20 16:47:25 -0600

    display first name each person

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,5 +2,11 @@
 
 puts 'EventManager Initialized!'
 
-contents = File.read 'event_attendees.csv'
-puts contents
+lines = File.readlines 'event_attendees.csv'
+lines.each_with_index do |line,index|
+  next if index.zero?
+
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
