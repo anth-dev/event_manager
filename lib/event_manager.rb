4bbec90f79d5d9b5aaa88c11d5ca8390436c4d94
Anthony Giles <omgliekpie@gmail.com>
2020-10-21 14:48:03 -0600

    switch to using the CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,8 +4,8 @@ require 'csv'
 
 puts 'EventManager Initialized!'
 
-contents = CSV.open 'event_attendees.csv', headers: true
-contents.each do |row|
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents.each do |:first_name|
   name = row[2]
   puts name
 end
