ea8b4c61b3068994cec9e27b07b711c06413f43b
Anthony Giles <omgliekpie@gmail.com>
2020-10-22 13:59:23 -0600

    change to use erb templating

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 
 def clean_zipcode(zipcode)
@@ -26,7 +27,9 @@ end
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-template_letter = File.read "form_letter.html"
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
@@ -35,8 +38,6 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME',name)
-  personal_letter.gsub!('LEGISLATORS',legislators)
-
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
